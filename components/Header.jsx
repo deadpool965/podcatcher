be8949973c6775f8d12b077ef60e13e81a0deebf
Thalles Maia <tmaiadev@gmail.com>
2019-05-10 10:26:00 -0300

    Fix Header style

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,12 +4,15 @@ function Header() {
       <h1>PODCATCHER</h1>
       <style jsx>
         {`
+          header {
+            padding: 16px 0px;
+          }
+
           h1 {
             font-family: 'Bungee', cursive !important;
             color: var(--accent-color);
             height: 36px;
-            padding-top: 16px;
-            padding-bottom: 8px;
+            margin: 0px;
           }
         `}
       </style>

b38ba8d0f8a05f845c1f880b8541264c056c43c1
Thalles Maia <tmaiadev@gmail.com>
2019-05-12 09:48:28 -0300

    TopCharts API

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.ENVIROMENT === 'production' ?
-  80 : 3001;
+const axios = require('axios');
+const { readFileSync } = require('fs');
+const NodeCache = require('node-cache');
 
-const indexFile = (() => {
-  const { readFileSync } = require('fs');
-  return readFileSync('./build/index.html', { encoding: 'utf-8' });
-})();
+const cache = new NodeCache({
+  stdTTL: 60 * 60, // hourly
+  checkperiod: 60 * 60,
+  deleteOnExpire: true,
+});
+const app = express();
+
+const PORT = process.env.ENVIROMENT === 'production'
+  ? 80 : 3001;
+
+const indexFile = (() => readFileSync('./build/index.html', { encoding: 'utf-8' }))();
+
+const fetch = async (url) => {
+  const cachedValue = cache.get(url);
+
+  if (cachedValue) {
+    return cachedValue;
+  }
+
+  const { data } = await axios(url);
+  cache.set(url, data, 60 * 60);
+  return data;
+};
 
 const lookup = (req, res) => res.send('lookup');
 const search = (req, res) => res.send('search');
-const topCharts = (req, res) => res.send('topCharts');
+const topCharts = async (req, res) => {
+  try {
+    const country = req.query.country || 'us';
+    const limit = req.query.limit || 50;
+    const data = await fetch(`https://rss.itunes.apple.com/api/v1/${country}/podcasts/top-podcasts/all/${limit}/explicit.json`);
+    res.send(data.feed.results);
+  } catch (e) {
+    res
+      .status(500)
+      .send(e);
+  }
+};
 
 app.get('/api/lookup', lookup);
 app.get('/api/search', search);

f0af50c1ec5b5c83dfe1edd4a46a94fbee3452f3
Thalles Maia <tmaiadev@gmail.com>
2019-05-13 22:19:02 -0300

    Fix lint

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -18,7 +18,7 @@ function App() {
             <Route
               path="/"
               exact
-              render={(props) => <DiscoveryPage {...props} />}
+              render={props => <DiscoveryPage {...props} />}
             />
           </main>
         </Container>

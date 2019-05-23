833be4b5891d1df0664f4f3d9f3da6b691913b15
Thalles Maia <tmaiadev@gmail.com>
2019-05-23 08:14:19 -0300

    Header title redirects to home when clicked

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
   return (
     <header className="header">
-      <h1 className="header__title">PODCATCHER</h1>
+      <Link
+        to="/"
+        className="header__title"
+      >
+        <h1 className="header__title__h1">PODCATCHER</h1>
+      </Link>
     </header>
   );
 }

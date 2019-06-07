c9435d01828ba6a48eca49f8e78109a799f71de8
Thalles Maia <tmaiadev@gmail.com>
2019-06-07 00:09:24 +0000

    Show offline message when podcast page requests dies

--- src/pages/Podcast/Podcast.jsx
+++ src/pages/Podcast/Podcast.jsx
@@ -8,6 +8,7 @@ import Grid from '../../components/Grid/Grid';
 import Modal from '../../components/Modal/Modal';
 import Button from '../../components/Button/Button';
 import Metadata from '../../components/Metadata/Metadata';
+import OfflineAlert from '../../components/OfflineAlert/OfflineAlert';
 import api from '../../libs/api';
 import './Podcast.css';
 
@@ -27,7 +28,9 @@ function PodcastPage({
   const { id } = match.params;
   const [search, setSearch] = useState('');
   const [data, setData] = useState({});
+  const [dataError, setDataError] = useState(false);
   const [episodes, setEpisodes] = useState([]);
+  const [episodesError, setEpisodesError] = useState(false);
   const [order, setOrder] = useState('DESC');
   const [showLimitMenu, setShowLimitMenu] = useState(false);
   const limit = match.params.limit || '25';
@@ -44,9 +47,11 @@ function PodcastPage({
     api(`podcast/${id}`)
       .then((res) => {
         setData(res);
-        return api(`episodes/${id}`);
+        api(`episodes/${id}`)
+          .then(e => setEpisodes(e))
+          .catch(() => setEpisodesError(true));
       })
-      .then(res => setEpisodes(res));
+      .catch(() => setDataError(true));
   }, [id]);
 
   function onImageLoad({ target }) {
@@ -76,6 +81,14 @@ function PodcastPage({
     history.push(`/${id}/${l}`);
     onLimitDialogClose();
   }
+
+  const genre = () => {
+    if (dataError) {
+      return 'Try again later';
+    }
+
+    return data.genres ? data.genres[0] : 'Loading...';
+  };
 
   return (
     <div className="podcast-page">
@@ -112,135 +125,153 @@ function PodcastPage({
       </Modal>
       <div className="podcast-page__summary">
         <div className="podcast-page__summary__image-wrapper">
-          <img
-            className="podcast-page__summary__image-wrapper__image"
-            src={data.artworkUrl600 || ''}
-            alt={`Logo of ${data.collectionName}` || 'Loading image...'}
-            onLoad={onImageLoad}
-          />
+          {dataError
+            ? <i className="icon ion-ios-wifi" aria-hidden />
+            : (
+              <img
+                className="podcast-page__summary__image-wrapper__image"
+                src={data.artworkUrl600 || ''}
+                alt={`Logo of ${data.collectionName}` || 'Loading image...'}
+                onLoad={onImageLoad}
+              />
+            )}
         </div>
         <div className="podcast-page__summary__content">
-          <h2 className="podcast-page__summary__content__title">{data.collectionName || ''}</h2>
+          <h2 className="podcast-page__summary__content__title">
+            {dataError
+              ? 'Oops!'
+              : data.collectionName || ''}
+          </h2>
           <div className="podcast-page__summary__content__artist">
-            {data.artistName || ''}
+            {dataError
+              ? 'Sorry, something went wrong. We could not reach the network'
+              : data.artistName || ''}
           </div>
           <div className="podcast-page__summary__content__genre">
-            {data.genres ? data.genres[0] : 'Loading...'}
+            {genre()}
           </div>
         </div>
       </div>
-      <div className="podcast-page__episodes">
-        <div className="podcast-page__episodes__content">
-          <form
-            style={{ marginBottom: '32px' }}
-            aria-label="Search for Episodes"
-          >
-            <Grid columns="auto min-content">
-              <TextInput
-                id="q"
-                name="q"
-                ariaLabel="Search Episode"
-                icon="search"
-                placeholder="Search Episode"
-                defaultValue={search}
-                onChange={handleSearchChange}
-                clearButton
-                onClear={handleSearchClear}
-                delayChange
-              />
-              <div className="podcast-page__search-tools">
-                <Button
-                  id="limit-btn"
-                  aria-label="Change Limit"
-                  onClick={() => setShowLimitMenu(true)}
-                  transparent
-                  lightText
-                  circle
-                >
-                  {limit}
-                </Button>
-                <Button
-                  id="order-btn"
-                  onClick={toggleOrder}
-                  aria-label="Change Order"
-                  transparent
-                  lightText
-                  circle
-                >
-                  <i
-                    className={
-                      'icon '
-                      + `ion-md-arrow-round-${order === 'DESC' ? 'down' : 'up'}`
-                    }
-                    aria-hidden
+      {dataError
+        ? null
+        : (
+          <div className="podcast-page__episodes">
+            <div className="podcast-page__episodes__content">
+              <form
+                style={{ marginBottom: '32px' }}
+                aria-label="Search for Episodes"
+              >
+                <Grid columns="auto min-content">
+                  <TextInput
+                    id="q"
+                    name="q"
+                    ariaLabel="Search Episode"
+                    icon="search"
+                    placeholder="Search Episode"
+                    defaultValue={search}
+                    onChange={handleSearchChange}
+                    clearButton
+                    onClear={handleSearchClear}
+                    delayChange
                   />
-                </Button>
-              </div>
-            </Grid>
-          </form>
-          <ul
-            className="podcast-page__episode-list"
-            aria-label="Podcast episodes"
-          >
-            {episodes.length === 0
-              ? <Spinner />
-              : episodes
-                .sort((a, b) => {
-                  const createdA = new Date(a.created);
-                  const createdB = new Date(b.created);
-
-                  if (order === 'ASC') {
-                    return createdA - createdB;
-                  }
-
-                  return createdB - createdA;
-                })
-                .filter((e) => {
-                  if (search === '') return true;
-
-                  const title = e.title
-                    .normalize('NFD')
-                    .replace(EQUALIZER_REG_EXP, '')
-                    .toLowerCase()
-                    .trim();
-
-                  if (title.indexOf(searchQuery) !== -1) {
-                    return true;
-                  }
-
-                  return false;
-                })
-                .filter((e, i) => {
-                  if (Number.isNaN(parseFloat(limit))) return true;
-                  return i < parseInt(limit, 10);
-                })
-                .map((episode) => {
-                  const { title, created } = episode;
-                  return (
-                    <li
-                      key={created || title}
-                      aria-label={title}
+                  <div className="podcast-page__search-tools">
+                    <Button
+                      id="limit-btn"
+                      aria-label="Change Limit"
+                      onClick={() => setShowLimitMenu(true)}
+                      transparent
+                      lightText
+                      circle
                     >
-                      <Episode episode={episode} />
-                    </li>
-                  );
-                })}
-          </ul>
-          {displayShowMoreButton
-            ? (
-              <div className="podcast-page__read-more">
-                <Link
-                  to={`/${id}/${parseInt(limit, 10) + 50}`}
-                  className="podcast-page__read-more__button"
-                >
-                  Show More Episodes
-                  <i className="podcast-page__read-more__button__icon icon ion-md-arrow-dropdown" />
-                </Link>
-              </div>
-            )
-            : null}
-        </div>
-      </div>
+                      {limit}
+                    </Button>
+                    <Button
+                      id="order-btn"
+                      onClick={toggleOrder}
+                      aria-label="Change Order"
+                      transparent
+                      lightText
+                      circle
+                    >
+                      <i
+                        className={
+                          'icon '
+                          + `ion-md-arrow-round-${order === 'DESC' ? 'down' : 'up'}`
+                        }
+                        aria-hidden
+                      />
+                    </Button>
+                  </div>
+                </Grid>
+              </form>
+              {episodesError
+                ? <OfflineAlert />
+                : (
+                  <ul
+                    className="podcast-page__episode-list"
+                    aria-label="Podcast episodes"
+                  >
+                    {episodes.length === 0
+                      ? <Spinner />
+                      : episodes
+                        .sort((a, b) => {
+                          const createdA = new Date(a.created);
+                          const createdB = new Date(b.created);
+
+                          if (order === 'ASC') {
+                            return createdA - createdB;
+                          }
+
+                          return createdB - createdA;
+                        })
+                        .filter((e) => {
+                          if (search === '') return true;
+
+                          const title = e.title
+                            .normalize('NFD')
+                            .replace(EQUALIZER_REG_EXP, '')
+                            .toLowerCase()
+                            .trim();
+
+                          if (title.indexOf(searchQuery) !== -1) {
+                            return true;
+                          }
+
+                          return false;
+                        })
+                        .filter((e, i) => {
+                          if (Number.isNaN(parseFloat(limit))) return true;
+                          return i < parseInt(limit, 10);
+                        })
+                        .map((episode) => {
+                          const { title, created } = episode;
+                          return (
+                            <li
+                              key={created || title}
+                              aria-label={title}
+                            >
+                              <Episode episode={episode} />
+                            </li>
+                          );
+                        })}
+                  </ul>
+                )}
+              {displayShowMoreButton
+                ? (
+                  <div className="podcast-page__read-more">
+                    <Link
+                      to={`/${id}/${parseInt(limit, 10) + 50}`}
+                      className="podcast-page__read-more__button"
+                    >
+                      Show More Episodes
+                      <i className="podcast-page__read-more__button__icon icon ion-md-arrow-dropdown" />
+                    </Link>
+                  </div>
+                )
+                : null}
+            </div>
+          </div>
+        )}
     </div>
   );
 }

2a5e3555e7d03d316b335cb5035baf2ddd7f9bc3
Thalles Maia <tmaiadev@gmail.com>
2019-06-11 09:52:51 -0300

    Proxy error handling

--- server/routes/proxy.js
+++ server/routes/proxy.js
@@ -1,14 +1,21 @@
 const request = require('request');
+const { parse } = require('url');
 
 module.exports = (req, res) => {
   const { url } = req.query;
-  if (!url) {
+
+  if (!url || !parse(url).hostname) {
     res
       .status(400)
-      .send('Query parameter \'url\' missing');
+      .send('Query parameter \'url\' missing or invalid');
     return;
   }
 
   request(url)
+    .on('error', () => {
+      res
+        .status(404)
+        .send('Invalid URL');
+    })
     .pipe(res);
 };
